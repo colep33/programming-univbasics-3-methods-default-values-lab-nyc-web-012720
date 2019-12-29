879c6977183372029fc1513001bd0e9754c1a46a
Cole Arthur Parham <coleparham@gmail.com>
2019-12-29 23:34:32 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -5,5 +5,6 @@ def meal_choice(veg1,veg2,protein = "meat")
     puts "What a nutritious meal!"
     phrase = "A plate of #{protein} with #{veg1} and #{veg2}."
     return phrase
+    puts phrase
   end
 end
