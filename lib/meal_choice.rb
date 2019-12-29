a1c6381e7e7ec7fca34854efc9412c9274df0159
Cole Arthur Parham <coleparham@gmail.com>
2019-12-29 23:27:33 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,7 +3,7 @@
 def meal_choice(veg1,veg2,protein = "meat")
   1.times do
     puts "What a nutritious meal!"
-    puts "A plate of #{protein} with #{veg1} and #{veg2}."
+    "A plate of #{protein} with #{veg1} and #{veg2}."
     return nil
   end
 end
